d2252e16f8ce32d825e141a2558590c6ea481d01
Duy <83393404+HoangDuyP@users.noreply.github.com>
2025-10-31 19:10:10 +0700

    Add information to homepage + minor change

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const newTask = document.createElement("div");
         newTask.className = "flex border border-sky-500 mb-2";
         newTask.innerHTML = `
-            <input id="taskContent${taskNo}" type="text" class="text-black grow pl-2" placeholder="New task..." />
+            <input type="text" class="text-black grow pl-2" placeholder="New task..." />
             <input id="checkDone${taskNo}" type="checkbox" class="largerCheckbox ml-2" />
             <button id="deleteTaskButton${taskNo}" class="text-center bg-red-500 w-5 ml-2">X</button>
         `;
